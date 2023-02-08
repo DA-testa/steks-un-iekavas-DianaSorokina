f80f4ddc5a0b68237ceb60ad69719b6b97b17390
Diana Sorokina <124806162+DianaSorokina@users.noreply.github.com>
2023-02-08 19:54:31 +0200

    -

--- main.py
+++ main.py
@@ -13,18 +13,27 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next,i))
             pass
 
         if next in ")]}":
-            # Process closing bracket, write your code here
+            if not opening_brackets_stack:
+                return i+1 
             pass
+            do = opening_brackets_stack.pop()
+            if not are_matching(do.char, next):
+                return i+1 
+
+    if opening_brackets_stack:
+        do = opening_brackets_stack.pop()
+        return top.position +1 
+    return "Success"      
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    print(mismatch)
 
 
 if __name__ == "__main__":
